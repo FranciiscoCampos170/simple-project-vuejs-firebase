24545f975f6a6961a7309b2287e23982f00c3bc1
Francisco Campos <franciiscocampos170@gmail.com>
2020-02-29 22:13:23 +0100

    Adicionando novos campos ao formulario

--- vuejs-firebase/src/components/Create.vue
+++ vuejs-firebase/src/components/Create.vue
@@ -3,8 +3,10 @@
 <div>
   <h1>Create Page</h1>
   <form @submit.prevent="addEmployer">
-    <input type="text" name="first-name" v-model="firstName">
-    <p>{{ firstName }}</p>
+    <input type="text" name="first-name" v-model="firstName" placeholder="First Name">
+    <input type="text" name="last-name" v-model="lastName" placeholder="Last Name">
+    <input type="text" name="email" v-model="email" placeholder="Email">
+    <button>Cadastrar</button>
   </form>
 </div>
 </template>
@@ -16,21 +18,27 @@ export default {
   props: ['name'],
   data(){
     return {
-      firstName: null
+      firstName: null,
+      lastName: null,
+      email: null
     }
   },
   methods: {
       addEmployer(){
-          //console.log(this.firstName);
-          if(this.firstName){
+          //console.log(this.firstName + this.lastName + this.email);
+          if(this.firstName && this.lastName && this.email){
             db.collection('employers').add({
-              content: this.firstName,
+              firstName: this.firstName,
+              lastName: this.lastName,
+              email: this.email,
               //name: this.name,
               timestamp: Date.now()
             }).catch(err => {
               console.log(err);
             })
-            this.firstName = null
+            this.firstName = null,
+            this.lastName = null,
+            this.email = null
           }else{
 
           }
